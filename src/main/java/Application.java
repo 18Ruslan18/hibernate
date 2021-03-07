2c01c95716a768edbe71933093b40b1ff46cffad
18Ruslan18 <79464524+18Ruslan18@users.noreply.github.com>
2021-03-08 02:18:52 +0300

    work hibernate witn Application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,13 @@
 import javax.security.auth.login.AppConfigurationEntry;
+
+import models.Car;
+import models.User;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
@@ -14,9 +19,19 @@ public class Application {
                 configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL95Dialect");
                 configuration.setProperty("hibernate.hbm2ddl.auto", "update");
                 configuration.addResource("User.hbm.xml");
-                //configuration.addAnnotatedClass(Car.class);
+                configuration.addAnnotatedClass(Car.class);
+                configuration.setProperty("hibernate.show_sql", "true");
                 configuration.setProperty("hibernate.show_sql", "true");
                 SessionFactory sessionFactory = configuration.buildSessionFactory();
+                Session session = sessionFactory.openSession();
+                User user = session.createQuery("from User user where user.id = 1", User.class).getSingleResult();
+
+                session.beginTransaction();
+                session.save(new User("Mini", "Max", 99));
+                session.getTransaction().commit();
+                System.out.println(user);
+
+                List<Car> car = session.createQuery("from Car car", Car.class).getResultList();
 
             }
         }
