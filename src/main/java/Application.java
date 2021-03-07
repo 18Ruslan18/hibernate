5016438e3ad20cf9dd3907adef4702ccd3e16752
18Ruslan18 <79464524+18Ruslan18@users.noreply.github.com>
2021-03-08 02:28:10 +0300

    end ORM

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -21,10 +21,9 @@ public class Application {
                 configuration.addResource("User.hbm.xml");
                 configuration.addAnnotatedClass(Car.class);
                 configuration.setProperty("hibernate.show_sql", "true");
-                configuration.setProperty("hibernate.show_sql", "true");
                 SessionFactory sessionFactory = configuration.buildSessionFactory();
                 Session session = sessionFactory.openSession();
-                User user = session.createQuery("from User user where user.id = 1", User.class).getSingleResult();
+                User user = session.createQuery("from User user where id=5", User.class).getSingleResult();
 
                 session.beginTransaction();
                 session.save(new User("Mini", "Max", 99));
@@ -32,6 +31,7 @@ public class Application {
                 System.out.println(user);
 
                 List<Car> car = session.createQuery("from Car car", Car.class).getResultList();
+                int i=0;
 
             }
         }
